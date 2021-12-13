0bd716182d977815ba981884732a99cf4f5f998d
Siddharth Telang <siddharth.telang910@gmail.com>
2021-12-13 10:21:50 -0500

    Add argparse in LGR Classifier

--- LogisticRegressionCLassifier.py
+++ LogisticRegressionCLassifier.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import OneHotEncoder
 from scipy.special import logsumexp
 from dimension_reduction import *
 from helper_functions import *
+import argparse
 
 class LogisticRegression:
 
@@ -17,7 +18,7 @@ class LogisticRegression:
     def fit(self, iter=1000, step=0.01, mu=0.01):
         self.loss_steps = self.gradient_descent(max_iter=iter, step=step, mu=mu)
 
-    def loss_plot(self, step):
+    def plot_loss(self, step):
         title = 'Training Data Size: ' + str(self.X.shape[0]) + ' Step size = '+str(step)
         return self.loss_steps.plot(
             title=title,
@@ -67,38 +68,56 @@ class LogisticRegression:
             W_lst.append(self.W)
             loss_lst.append(self.loss())
 
-        df = pd.DataFrame({
+        dataframe = pd.DataFrame({
             'step': step_lst, 
             'loss': loss_lst
         })
-        return df
+        return dataframe
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-training', '--training',required=False, default=10000, type=int)
+    parser.add_argument('-testing', '--testing',required=False, default=1000, type=int)
+    parser.add_argument('-step', '--step',required=False, default=0.1, type=float)
+    parser.add_argument('-pca', '--pca',required=False, default=False, type=bool)
+    parser.add_argument('-mda', '--mda',required=False, default=True, type=bool)
+    parser.add_argument('-iter', '--iter',required=False, default=100, type=int)
+    parser.add_argument('-reg', '--reg',required=False, default='0.01', type=float)
+    parser.add_argument('-random', '--random',required=False, default=50000, type=int)
+    args = vars(parser.parse_args())
+
+
+    n_train = args['training']
+    n_test = args['testing']
+    step_size = args['step']
+    usePca = args['pca']
+    useMDA = args['mda']
+    iterations = args['iter']
+    regularization = args['reg']
+    random = args['random']
+
+    # get training and testing data
     training_data, y_train, testing_data, y_test = get_training_testing_data()
     flattened = np.array([training_data[i].flatten() for i in range(training_data.shape[0])])
     
     # Perform PCA/MDA whichever true
-    usePca = False
-    useMDA = True
+    print('Reducing dimensions......')
     if usePca:
         projected = doPCA(flattened)
     else:
         projected = doMDA(flattened, y_train, dim=9)
     
-    # fit model
-    Y = y_train
-    n_train = 10000
-    n_test = 1000
-    step_size = 0.1
-    iterations = 500
-    regularization = 0.01
+    # train model
 
+    print('Fitting the Logistic Regression model')
+    Y = y_train
     model = LogisticRegression(projected[:n_train], Y[:n_train])
     model.fit(iter=iterations, step=step_size, mu=regularization)
-    model.loss_plot(step_size)
+    model.plot_loss(step_size)
     plt.show()
+
     # predict
-    pred = model.predict(projected[50000 : 50000 + n_test]) == y_train[50000 : 50000 + n_test]
+    pred = model.predict(projected[random : random + n_test]) == y_train[random : random + n_test]
     accuracy = np.where(pred == True)[0].shape[0] * 100 / n_test
     print('Accuracy of model = ', str(accuracy))
