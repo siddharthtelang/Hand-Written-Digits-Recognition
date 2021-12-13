ce6c81271cabda5bda9bf382209e66593ff7c21c
Siddharth Telang <siddharth.telang910@gmail.com>
2021-12-13 01:25:15 -0500

    Update SVM Classifier

--- svmClassifier.py
+++ svmClassifier.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from dimension_reduction import *
 from helper_functions import *
+import argparse
 
 def svm_classifier(projected_train, projected_test, y_train, y_test,\
                    train, test, kernel, gamma=0.05, margin=0.5, degree=3):
@@ -47,27 +48,28 @@ def svm_classifier(projected_train, projected_test, y_train, y_test,\
                 acc.append(accuracy)
 
 
-def reduce_dimension(usePCA, X_train, y_train, X_test, y_test):
-    if usePCA:
-        projected_train = doPCA(X_train)
-        projected_test = doPCA(X_test, dim=projected_train.shape[1])
-    else:
-        projected_train = doMDA(X_train, y_train, classes-1)
-        projected_test = doMDA(X_test, y_test, classes-1)
-    
-    return projected_train, projected_test
-
-
 if __name__ == '__main__':
 
-    usePCA = False
-    useMDA = True
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-training', '--training',required=False, default=5000, type=int)
+    parser.add_argument('-testing', '--testing',required=False, default=1000, type=int)
+    parser.add_argument('-kernel', '--kernel',required=False, default='rbf', type=str)
+    parser.add_argument('-pca', '--pca',required=False, default=False, type=bool)
+    parser.add_argument('-mda', '--mda',required=False, default=True, type=bool)
+    parser.add_argument('-gamma', '--gamma',required=False, default='0.05', type=float)
+    parser.add_argument('-margin', '--margin',required=False, default='0.5', type=float)
+    parser.add_argument('-degree', '--degree',required=False, default='3', type=int)
+    args = vars(parser.parse_args())
+
+    usePCA = args['pca']
+    useMDA = args['mda']
+    train = args['training']
+    test = args['testing']
+    kernel = args['kernel']
+    gamma = args['gamma']
+    degree = args['degree']
+    margin = args['margin']
     classes = 10
-    train = 20000
-    test = 5000
-    # kernel = ['poly', 'rbf', 'linear', 'sigmoid']
-    kernel = ['poly']
-    gamma = 0.05
 
     training_data, y_train, testing_data, y_test = get_training_testing_data()
 
@@ -77,15 +79,14 @@ if __name__ == '__main__':
 
     # dimension reduction
     print('Reducing dimensions....')
-    projected_train, projected_test = reduce_dimension(usePCA, X_train, y_train, X_test, y_test)
+    projected_train, projected_test = reduce_dimension(usePCA, X_train, y_train, X_test, y_test, classes)
     
     print('Dimensions reduced, proceed for classification...')
 
     print('Kernel\tMargin\tGamma\t Degree\tAccuracy')
 
-    for k in kernel:
-        svm_classifier(projected_train, projected_test, y_train, y_test,\
-                       train, test, k, gamma=0.05, margin=0.5, degree=3)
+    svm_classifier(projected_train, projected_test, y_train, y_test,\
+                       train, test, kernel, gamma=gamma, margin=margin, degree=degree)
 
 
     
